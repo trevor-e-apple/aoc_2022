116d90fd531aa8dc69aa63d9d8eff2da136cee31
Trevor Apple <trappleco@gmail.com>
2023-01-06 19:28:24 -0800

    day fourteen solution

--- day_fourteen/src/main.rs
+++ day_fourteen/src/main.rs
@@ -8,7 +8,42 @@ struct Sand {
     pos: (i32, i32),
 }
 
-// DOWN IS POSITIVE FOR Y (easier to draw)
+fn check_block(
+    point: (i32, i32), all_rocks: &Vec<Rock>, all_sand: &Vec<Sand>
+) -> bool {
+    // check if sand hit another piece of sand
+    let mut blocked = false;
+    for check_sand in all_sand {
+        if check_sand.pos == point {            
+            blocked = true;
+            break;
+        }
+    }
+
+    // check if sand hit a rock
+    if !blocked {
+        for rock in all_rocks {
+            if rock.check_rock(point) {
+                blocked = true;
+                break;
+            }
+        }
+
+    }
+    blocked
+}
+
+fn check_abyss(point: (i32, i32), all_rocks: &Vec<Rock>) -> bool {
+    for rock in all_rocks {
+        for rock_point in &rock.points {
+            if point.1 < rock_point.1 {
+                return false;
+            }
+        }
+    }
+
+    true
+}
 
 /// prints a 20x20 area below the current grain of sand
 fn print_state(sand: &Sand, all_rocks: &Vec<Rock>, all_sand: &Vec<Sand>) {
@@ -89,42 +124,43 @@ fn main() {
     print_state(&Sand { pos: SAND_START }, &rocks, &all_sand);
 
     loop {
+        let mut in_abyss = false;
         let mut sand = Sand { pos: SAND_START };
         loop {
             // TODO: check for falling into the abyss
-            // TODO: sand flow
-            let next_pos = (sand.pos.0, sand.pos.1 + 1);
+            let mut next_pos = (sand.pos.0, sand.pos.1 + 1);
 
-            // check if sand hit another piece of sand
-            let mut hit_sand = false;
-            for check_sand in &all_sand {
-                if check_sand.pos == next_pos {
-                    hit_sand = true;
-                    break;
+            // try just below
+            let mut stuck = check_block(next_pos, &rocks, &all_sand);
+            if stuck {
+                // try left
+                next_pos = (sand.pos.0 - 1, sand.pos.1 + 1);
+                stuck = check_block(next_pos, &rocks, &all_sand);
+                if stuck {
+                    // try right
+                    next_pos = (sand.pos.0 + 1, sand.pos.1 + 1);
+                    stuck = check_block(next_pos, &rocks, &all_sand);
                 }
             }
 
-            // check if sand hit a rock
-            let mut hit_rock = false;
-            for rock in &rocks {
-                if rock.check_rock(next_pos) {
-                    hit_rock = true;
-                    break;
-                }
-            }
-
-            if !(hit_sand || hit_rock) {
+            if !stuck {
                 sand.pos = next_pos;
+                if check_abyss(sand.pos, &rocks) {
+                    in_abyss = true;
+                    break;
+                }
             } else {
                 break;
             }
 
            print_state(&sand, &rocks, &all_sand);
-           sleep(Duration::from_secs(1));
+           sleep(Duration::from_millis(17));
         }
 
         if sand.pos == SAND_START {
             // could not move the sand
+            break;
+        } else if in_abyss {
             break;
         } else {
             all_sand.push(sand);
